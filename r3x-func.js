67b60fa459d060cb83c3e3625b31337abfc2969d
ciaranRoche <ciaranroche@hotmail.co.uk>
2019-02-04 19:45:39 +0000

    update showcase to latest cli, sdk version

--- r3x-func.js
+++ r3x-func.js
@@ -2,6 +2,6 @@ const r3x = require('@rubixfunctions/r3x-js-sdk')
 
 let schema
 r3x.execute(function(){
-    let response = {'message' : 'Hello r3x function'}
-    return response 
+	let response = {'message' : 'Hello r3x function'}
+	return response 
 }, schema)
