f7e8d42e8febfba85eb2a7947ca2c4854994d2ed
ciaranRoche <ciaranroche@hotmail.co.uk>
2019-03-11 18:20:06 +0000

    add react scafolding

---
+++ rubix-notes-node/README.md
@@ -0,0 +1,9 @@
+# RubiX Showcase SPA
+
+This directory contain a SPA sample application built to demonstrate the RubiX Framework
+
+## Documentation
+For full details on how deploy this function to Knative, please refer to our [Documentation here.](https://github.com/rubixFunctions/r3x-docs/blob/master/README.md)
+
+## License
+This project is licensed under the Apache 2.0 License - see the [LICENSE](LICENSE) file for details
---
+++ rubix-notes-node/src/App.js
@@ -0,0 +1,132 @@
+import React from 'react';
+import {
+  BackgroundImage,
+  BackgroundImageSrc,
+  Brand,
+  Nav,
+  NavGroup,
+  NavItem,
+  Page,
+  PageHeader,
+  PageSection,
+  PageSectionVariants,
+  PageSidebar,
+  TextContent,
+  Text,
+  Toolbar,
+} from '@patternfly/react-core';
+import brandImg from './l_pf-reverse-164x11.png';
+
+class PageLayoutGroupsNav extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDropdownOpen: false,
+      isKebabDropdownOpen: false,
+      activeItem: 'grp-1_itm-1'
+    };
+  }
+
+  onDropdownToggle = isDropdownOpen => {
+    this.setState({
+      isDropdownOpen
+    });
+  };
+
+  onDropdownSelect = event => {
+    this.setState({
+      isDropdownOpen: !this.state.isDropdownOpen
+    });
+  };
+
+  onKebabDropdownToggle = isKebabDropdownOpen => {
+    this.setState({
+      isKebabDropdownOpen
+    });
+  };
+
+  onKebabDropdownSelect = event => {
+    this.setState({
+      isKebabDropdownOpen: !this.state.isKebabDropdownOpen
+    });
+  };
+
+  onNavSelect = result => {
+    this.setState({
+      activeItem: result.itemId
+    });
+  };
+
+  render() {
+    const { activeItem } = this.state;
+
+    const PageNav = (
+      <Nav onSelect={this.onNavSelect} aria-label="Nav">
+        <NavGroup title="RubiX">
+          <NavItem to="#grouped-1" itemId="grp-1_itm-1" isActive={activeItem === 'grp-1_itm-1'}>
+            Documentation
+          </NavItem>
+          <NavItem to="#grouped-2" itemId="grp-1_itm-2" isActive={activeItem === 'grp-1_itm-2'}>
+            Command Line Tool
+          </NavItem>
+          <NavItem to="#grouped-3" itemId="grp-1_itm-3" isActive={activeItem === 'grp-1_itm-3'}>
+            Software Development Kit
+          </NavItem>
+        </NavGroup>
+        <NavGroup title="Knative">
+          <NavItem to="#grouped-7" itemId="grp-2_itm-1" isActive={activeItem === 'grp-2_itm-1'}>
+            About
+          </NavItem>
+          <NavItem to="#grouped-8" itemId="grp-2_itm-2" isActive={activeItem === 'grp-2_itm-2'}>
+            Documentation
+          </NavItem>
+        </NavGroup>
+      </Nav>
+    );
+    const PageToolbar = (
+      <Toolbar>
+    
+      </Toolbar>
+    );
+    const bgImages = {
+      [BackgroundImageSrc.lg]: '/assets/images/pfbg_1200.jpg',
+      [BackgroundImageSrc.sm]: '/assets/images/pfbg_768.jpg',
+      [BackgroundImageSrc.sm2x]: '/assets/images/pfbg_768@2x.jpg',
+      [BackgroundImageSrc.xs]: '/assets/images/pfbg_576.jpg',
+      [BackgroundImageSrc.xs2x]: '/assets/images/pfbg_576@2x.jpg',
+      [BackgroundImageSrc.filter]: '/assets/images/background-filter.svg#image_overlay'
+    };
+
+    const Header = (
+      <PageHeader
+        logo={<Brand src={brandImg} alt="Patternfly Logo" />}
+        toolbar={PageToolbar}
+        showNavToggle
+      />
+    );
+    const Sidebar = <PageSidebar nav={PageNav} />;
+
+    return (
+      <React.Fragment>
+        <BackgroundImage src={bgImages} />
+        <Page header={Header} sidebar={Sidebar} isManagedSidebar>
+          <PageSection variant={PageSectionVariants.light}>
+            <TextContent>
+              <Text component="h1">Main Title</Text>
+              <Text component="p">
+                Body text should be Overpass Regular at 16px. It should have leading of 24px because <br />
+                of it’s relative line height of 1.5.
+              </Text>
+            </TextContent>
+          </PageSection>
+          <PageSection variant={PageSectionVariants.light}>Light</PageSection>
+          <PageSection variant={PageSectionVariants.dark}>Dark</PageSection>
+          <PageSection variant={PageSectionVariants.darker}>Darker</PageSection>
+          <PageSection>Content</PageSection>
+        </Page>
+      </React.Fragment>
+    );
+  }
+}
+
+export default PageLayoutGroupsNav;
