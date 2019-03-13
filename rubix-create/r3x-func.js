1a190a8f286a8ccbef7f8a8d4a25e727e662b5d3
ciaranRoche <ciaranroche@hotmail.co.uk>
2019-03-13 17:34:29 +0000

    add initial datastore func

--- rubix-create/r3x-func.js
+++ rubix-create/r3x-func.js
@@ -1,7 +1,38 @@
 const r3x = require('@rubixfunctions/r3x-js-sdk')
+const {Datastore} = require('@google-cloud/datastore');
+
+async function addClip() {
+	// Your Google Cloud Platform project ID
+	const projectId = 'r3x-showcase-42';
+  
+	// Creates a client
+	const datastore = new Datastore({
+	  projectId: projectId,
+	});
+  
+	// The kind for the new entity
+	const kind = 'Clip';
+	// The name/ID for the new entity
+	const name = 'rubixClip';
+	// The Cloud Datastore key for the new entity
+	const taskKey = datastore.key([kind, name]);
+  
+	// Prepares the new entity
+	const task = {
+	  key: taskKey,
+	  data: {
+		clipId: 'clip1',
+		clip: 'Wubba lubba dub dub',
+	  },
+	};
+  
+	// Saves the entity
+	await datastore.save(task);
+	let response = {'message' : `Saved ${task.key.name}: ${task.data.clip}`}
+	return response 
+  }
 
 let schema
 r3x.execute(function(){
-	let response = {'message' : 'Hello r3x function'}
-	return response 
+	return addClip().catch(console.error);
 }, schema)
