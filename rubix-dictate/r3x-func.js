34a8e9c056e117689fbd26139c022b01ca45c00f
ciaranRoche <ciaranroche@hotmail.co.uk>
2019-03-13 10:01:24 +0000

    add initial dictate function

--- rubix-dictate/r3x-func.js
+++ rubix-dictate/r3x-func.js
@@ -1,7 +1,37 @@
 const r3x = require('@rubixfunctions/r3x-js-sdk')
+// Imports the Google Cloud client library
+const textToSpeech = require('@google-cloud/text-to-speech');
+
+// Import other required libraries
+const fs = require('fs');
+const util = require('util');
 
 let schema
 r3x.execute(function(){
-	let response = {'message' : 'Hello r3x function'}
-	return response 
+	return dictate() 
 }, schema)
+
+async function dictate() {
+  // Creates a client
+  const client = new textToSpeech.TextToSpeechClient();
+
+  // The text to synthesize
+  const text = 'Hello, world!';
+
+  // Construct the request
+  const request = {
+    input: {text: text},
+    // Select the language and SSML Voice Gender (optional)
+    voice: {languageCode: 'en-US', ssmlGender: 'NEUTRAL'},
+    // Select the type of audio encoding
+    audioConfig: {audioEncoding: 'MP3'},
+  };
+
+  // Performs the Text-to-Speech request
+  const [response] = await client.synthesizeSpeech(request);
+  // Write the binary audio content to a local file
+  const writeFile = util.promisify(fs.writeFile);
+  await writeFile('output.mp3', response.audioContent, 'binary');
+  let res = {'message' : 'Audio content written to file: output.mp3'}
+  return res
+}
