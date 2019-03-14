5c03331cfbffbec8b495981d209efc372d2b67c7
ciaranRoche <ciaranroche@hotmail.co.uk>
2019-03-14 09:10:29 +0000

    add initial delete function

--- rubix-delete/r3x-func.js
+++ rubix-delete/r3x-func.js
@@ -1,7 +1,25 @@
 const r3x = require('@rubixfunctions/r3x-js-sdk')
+const {Datastore} = require('@google-cloud/datastore');
+
+async function deleteClip(clipId) {
+	const projectId = 'r3x-showcase-42';
+  
+	// Creates a client
+	const datastore = new Datastore({
+	  projectId: projectId,
+	});
+
+	const ClipKey = datastore.key(['Clips', clipId]);
+
+	await datastore.delete(ClipKey);
+	console.log(`Task ${clipId} deleted successfully.`);
+}
 
 let schema
-r3x.execute(function(){
+r3x.execute(function(input){
+	if (input.clipId){
+		deleteClip(input.clipId)
+	} 
 	let response = {'message' : 'Hello r3x function'}
 	return response 
 }, schema)
